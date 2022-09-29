cc361c225c03828e8fc326156c128c9fcf688eff
samumirandam <samuelchampi@gmail.com>
2022-09-29 16:56:58 -0500

    feat: added login

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,5 @@
+import { ProviderAuth } from "@hooks/useAuth";
+
 import MainLayout from "@layout/MainLayout";
 
 import "@styles/tailwind.css";
@@ -5,9 +7,11 @@ import "@styles/tailwind.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+      <ProviderAuth>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </ProviderAuth>
     </>
   );
 }

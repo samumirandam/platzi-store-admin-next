0438096321322a7313ecdf6f61c006d0f70d4068
samumirandam <samuelchampi@gmail.com>
2022-09-29 11:02:14 -0500

    feat: added ui components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 
 const colors = require("tailwindcss/colors");
 
+delete colors["lightBlue"];
+delete colors["warmGray"];
+delete colors["trueGray"];
+delete colors["coolGray"];
+delete colors["blueGray"];
+
 module.exports = {
   content: ["./src/**/*{html,js,jsx}"],
   theme: {
